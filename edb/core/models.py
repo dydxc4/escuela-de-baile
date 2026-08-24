9bef35ab6d5eb42fe3186f6e1c0a5c5743731427
Danny Diaz <danysadiaz4@gmail.com>
2026-08-24 15:08:31 -0700

    Corrección de listas con datos anidados opcionales en Clase y Cuota

--- edb/core/models.py
+++ edb/core/models.py
@@ -121,7 +121,7 @@ class Clase(models.Model):
     duracion = models.DurationField()
     descripcion = models.CharField(max_length=120, null=True, blank=True)
     estado = EnumField(Estado, default=Estado.PENDIENTE)
-    estudiantes = models.ManyToManyField(Estudiante, related_name='clases')
+    estudiantes = models.ManyToManyField(Estudiante, related_name='clases', null=True, blank=True)
 
     def __str__(self) -> str:
         return f'{self.periodo} - Clase {self.fecha_hora} [{self.estado}]'
@@ -141,7 +141,7 @@ class Cuota(models.Model):
     fecha_registro = models.DateTimeField(auto_now_add=True)
     fecha_actualizacion = models.DateTimeField(auto_now=True)
     esta_habilitado = models.BooleanField(default=True)
-    clases = models.ManyToManyField(Clase, related_name='cuotas')
+    clases = models.ManyToManyField(Clase, related_name='cuotas', null=True, blank=True)
 
     def __str__(self) -> str:
         return f'{self.concepto}: ${self.costo}'
